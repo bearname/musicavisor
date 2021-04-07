4a2b84854d799c77c89d15650445ab93d3174603
a <ya.mikushoff@gmail.com>
2021-04-07 13:17:16 +0300

    add export to json file functionality

--- src/main/java/ru/mikushov/musicadvisor/service/JsonExportService.java
+++ src/main/java/ru/mikushov/musicadvisor/service/JsonExportService.java
@@ -1,10 +1,16 @@
 package ru.mikushov.musicadvisor.service;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import ru.mikushov.musicadvisor.model.Album;
 import ru.mikushov.musicadvisor.model.Music;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class JsonExportService implements ExportService {
@@ -19,12 +25,24 @@ public class JsonExportService implements ExportService {
         System.out.println("export");
         Gson gson = new Gson();
         List<Album> newReleasesMusic = musicService.getNewReleasesMusic();
-        System.err.println(gson.toJson(newReleasesMusic));
-//        newReleasesMusic.forEach(System.out::println);
+        try {
+            try (Writer writer1 = new FileWriter(getExportFileName())) {
+                Gson gson1 = new GsonBuilder()
+                        .setPrettyPrinting()
+                        .create();
+                gson1.toJson(newReleasesMusic, writer1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void load(String filename) {
 
+    }
+
+    private String getExportFileName() {
+        return "C:\\Users\\mikha\\Desktop\\github\\musicadvisor\\dump-" + new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date()) + ".json";
     }
 }
