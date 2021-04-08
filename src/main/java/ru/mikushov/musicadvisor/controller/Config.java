3f5f9395b34133a4e7842ebe7ceb1558bc46b91d
a <ya.mikushoff@gmail.com>
2021-04-08 05:27:50 +0300

    fix

--- src/main/java/ru/mikushov/musicadvisor/controller/Config.java
+++ src/main/java/ru/mikushov/musicadvisor/controller/Config.java
@@ -6,7 +6,8 @@ public class Config {
             "categories — a list of all available categories on Spotify (just their names);\n" +
             "playlists C_NAME, where C_NAME is the name of category. The list contains playlists of this category and their links on Spotify;\n" +
             "exit shuts down the application.";
-    public static final String CLIENT_ID = "56c9c163d86e4522879985ad33d65805";
-    public static final String CLIENT_SECRET = "0746a39525aa4ade9ce380a840b929c0";
+
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
     public static final String REDIRECT_URI = "http://localhost:8080/";
 }
